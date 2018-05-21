c926e1a336448651addf5dc693b768dbcae73d1e
Eduardo Garcia <eagarcia8@gmail.com>
2018-05-21 19:59:37 +0000

    Added volunteer save function

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,1 +1,24 @@
-TESTING!
+/* global $ */
+
+$(document).ready(() => {
+    
+    console.log('TEST!');
+    
+    var volunteerButton = $("#volunteer-submit");
+    
+    volunteerButton.click(() => {
+        var name = $("#volunteer-name").val();
+        var email = $("#volunteer-email").val();
+        var address = $("#volunteer-address").val();
+        
+        var volunteer = {
+            "name": name,
+            "email": email,
+            "address": address
+        }
+        
+        // UPDATE URL TO OFFICIAL SERVER
+        $.post("https://angular-test-eagarcia.c9users.io/submitVolunteer", volunteer);
+    });
+    
+});
