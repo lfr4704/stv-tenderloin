1d48dc893618a1f4477b1feddae6ae852cb50b0e
Eduardo Garcia <eagarcia8@gmail.com>
2018-05-30 18:01:10 +0000

    Split up website into components

--- stv-angular/src/app/app.module.ts
+++ stv-angular/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { AboutComponent } from './about/about.component';
+import { EventsComponent } from './events/events.component';
+import { VolunteerComponent } from './volunteer/volunteer.component';
+import { DonateComponent } from './donate/donate.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProgramsComponent } from './programs/programs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    AboutComponent,
+    EventsComponent,
+    VolunteerComponent,
+    DonateComponent,
+    ContactComponent,
+    ProgramsComponent
   ],
   imports: [
     BrowserModule
