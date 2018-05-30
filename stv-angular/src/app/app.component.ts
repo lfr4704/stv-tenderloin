7a8faf39226cabbcaa345ba1fe4f0b7e23c52116
Eduardo Garcia <eagarcia8@gmail.com>
2018-05-30 17:08:07 +0000

    split navigation component

--- stv-angular/src/app/app.component.ts
+++ stv-angular/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Stop The Violence Angular site';
+  title = 'Stop The Violence Angular Site';
 }
